05f14887944a61b92939dc7b9943f8dca407889e
Wesley Bernardes <bw3sley@gmail.com>
2026-04-10 21:51:28 -0300

    ci: update workflow

--- iam.tf
+++ iam.tf
@@ -135,6 +135,12 @@ resource "aws_iam_role_policy" "tf-app-permissions" {
         Action   = "iam:*"
         Effect   = "Allow"
         Resource = "*"
+      },
+      {
+        Sid      = "Statement3"
+        Action   = "s3:*"
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
   })
--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.39.0"
     }
   }
+
+  backend "s3" {
+    bucket = "rocketseat-iac-bw3sley"
+    key    = "state/terraform.tfstate"
+    region = "us-east-2"
+  }
 }
 
 variable "aws_profile" {
@@ -14,4 +20,24 @@ variable "aws_profile" {
 provider "aws" {
   region  = "us-east-2"
   profile = var.aws_profile != "" ? var.aws_profile : null
+}
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-iac-bw3sley"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-iac-bw3sley"
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
